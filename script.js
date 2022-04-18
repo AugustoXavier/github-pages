a3f45d33218f4dfe26d3fd08b5199e6aedeb579e
AugustoXavier <42501693+AugustoXavier@users.noreply.github.com>
2022-04-18 12:47:25 -0300

    segundo teste

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ botao.addEventListener("click",ligareDesliga);
 function ligareDesliga(){
     
         let lampada = document.getElementById('luz').src;
-        let lamligada='http://127.0.0.1:5500/img/lam-ligada.png'
-        let lamdesligada='http://127.0.0.1:5500/img/lam-desligada.png'
+        let lamligada='https://raw.githubusercontent.com/AugustoXavier/github-pages/main/lam-ligada.png'
+        let lamdesligada='https://raw.githubusercontent.com/AugustoXavier/github-pages/main/lam-desligada.png'
     
         if(lampada===lamligada){
             
